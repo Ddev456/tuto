96bf6bf9b024cb34fe4c50b8956becdeffee61cb
Dylan DELHALLE <74488220+Ddev456@users.noreply.github.com>
2025-05-27 11:19:55 +0200

    :sparkles: tailwindcss setup 2 (#51)

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <p>Hello World</p>
-  </div>
+  <h1 class="text-xs font-extrabold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-secondary">
+    <span>Test</span>
+    <Icon name="tabler:map" />
+  </button>
 </template>
